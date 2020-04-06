393ec3d3ad6375a8c14654ebc32ece3f14e81fd6
Mohaimeen Hossain <mhossain5464@gmail.com>
2020-04-06 18:31:14 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -11,7 +11,14 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-
+if ( age > 12 && age < 20) {
+    return 'You are a teenager!'
+  }
+else if ( age < 13) {
+   return 'You are a kid'
+ }
+else if ( age > 19) {
+     return 'You are a grownup'}
 }
 
 function ternaryTeenager(age) {
